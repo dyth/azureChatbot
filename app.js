84739babeaad69906a88dc7000dc7838c75728bf
wmws2 <wmws2@cam.ac.uk>
2017-01-28 23:18:37 +0000

    Update app.js

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ bot.dialog('/', [
         }
     },
     function (session, results) {
-		n = 3
+	var n = 3;
         session.send("Hello %s, here's a random number!" + n.toString(), session.userData.name);
     }
 ]);
