4226853b7e51d3ec3e5bf53a7af7464b419b1899
wmws2 <wmws2@cam.ac.uk>
2017-01-29 00:00:01 +0000

    Update app.js

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ bot.dialog('/profile', [
 bot.dialog('/checkintent', [
 	function (session) {
 		builder.Prompts.text(session, 'Hello $name, how can I help you today?');
-	}
+	},
 	function (session, results) {
 		var response = results.response;
 		session.send('Your response was %s', response);
