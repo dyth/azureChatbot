28e00c1e1b483de951b283b37fca7644cf6962d4
David Hui <dyth2@cam.ac.uk>
2017-01-29 01:46:27 +0000

    added brute nlp

---
+++ category.js
@@ -0,0 +1,36 @@
+function greatest(array) {
+	var greatest = 0;
+	var index = 0;
+	for (var i = 0; i < array.length; i++) {
+		if (greatest < array[i]) {
+			index = i;
+			greatest = array[i];
+		}
+	}
+	return index;
+}
+
+var sentence = "Can we change topics?";
+
+function category(sentence) {
+	var topics = [["hello", "hey", "how", "sup", "good", "hi", "pleased", "what\'s"], ["change", "topics", "subject", "different"], ["don\'t", "know", "hint", "suggestion", "what", "mean", "unsure", "strange"], ["joke", "laugh", "funny", "humour"]];
+	var categories = ["greetings", "confusion", "topics", "jokes"];
+	var words = sentence.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/,"").toLowerCase().split(" ");
+	var counting = [];
+
+	for (count = 0; count < 4; count++) {
+		var score = 0;
+		for (i = 0; i < words.length; i++) {
+			// words into lower case .toLowerCase()
+			if (topics[count].includes(words[i])) {
+				score++;
+			}
+		}
+		// compute score of how many are in topic.
+		counting.push(score);
+	}
+	// print topic
+	return categories[greatest(counting)];
+}
+
+console.log(category(sentence))
