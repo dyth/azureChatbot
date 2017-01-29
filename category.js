14b911ccba1f68a4c56c2171a9524d605ec6769d
David Hui <dyth2@cam.ac.uk>
2017-01-29 02:03:49 +0000

    added more classes

--- category.js
+++ category.js
@@ -11,12 +11,12 @@ function greatest(array) {
 }
 
 function category(sentence) {
-	var topics = [["hello", "hey", "how", "sup", "good", "hi", "pleased", "what\'s"], ["change", "topics", "subject", "different"], ["don\'t", "know", "hint", "suggestion", "what", "mean", "unsure", "strange"], ["joke", "laugh", "funny", "humour"]];
-	var categories = ["greetings", "confusion", "subject", "jokes"];
+	var topics = [["hello", "hey", "how", "sup", "good", "hi", "pleased", "what\'s"], ["change", "topics", "subject", "different"], ["don\'t", "know", "hint", "suggestion", "what", "mean", "unsure", "strange"], ["joke", "laugh", "funny", "humour"], ["stop", "enough", "quit", "halt", "no", "end", "finish"]];
+	var categories = ["Greetings!", "Hint!", "Subject!", "Jokes!", "Quit!"];
 	var words = sentence.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/,"").toLowerCase().split(" ");
 	var counting = [];
 
-	for (count = 0; count < 4; count++) {
+	for (count = 0; count < categories.length; count++) {
 		var score = 0;
 		for (i = 0; i < words.length; i++) {
 			// words into lower case .toLowerCase()
@@ -28,5 +28,24 @@ function category(sentence) {
 		counting.push(score);
 	}
 	// print topic
-	return categories[greatest(counting)];
+	if (counting[greatest(counting)] == 0) {
+		return "Confused!";
+	} else {
+		return categories[greatest(counting)];
+	}
 }
+
+function subject(sentence) {
+	if (category == "topic") {
+		var subject = ["mathematics", "maths", "sum"]
+		var words = sentence.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/,"").toLowerCase().split(" ");
+		for (i = 0; i < worlds.length; i++) {
+			if (subject.includes([word])) {
+				return "Mathematics!";
+			}
+		}
+		return "Physics!"
+	}
+}
+
+console.log(category("My hovercraft is full of eels"))
